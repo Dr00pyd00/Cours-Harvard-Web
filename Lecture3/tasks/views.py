4939c3e49486c08b6368843d5dcaf0398b8a7e7a
kottak <rhapsodyluna1996@gmail.com>
2025-04-19 22:28:40 +0300

    app tasks done

--- Lecture3/tasks/views.py
+++ Lecture3/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+tasks = ['foo', 'bar', 'baz']
+
+def index(request):
+    ctx = {'tasks':tasks}
+    return render(request, 'tasks/index.html', ctx)
