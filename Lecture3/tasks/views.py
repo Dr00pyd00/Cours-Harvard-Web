2c30cc127256977f177cce98043dc42219ef9eb9
kottak <rhapsodyluna1996@gmail.com>
2025-04-20 12:12:53 +0300

    lesson Django CS50 finished

--- Lecture3/tasks/views.py
+++ Lecture3/tasks/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
 
 # Create your views here.
-tasks = ['foo', 'bar', 'baz']
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label='New task')
+
+
 
 def index(request):
-    ctx = {'tasks':tasks}
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+
+    ctx = {'tasks':request.session['tasks']}
     return render(request, 'tasks/index.html', ctx)
+
+def add(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task]
+            return redirect('tasks:index')
+            
+        else:
+            ctx = {'form':form}
+            return render(request, 'tasks/add.html', ctx)
+        
+    ctx = {'form': NewTaskForm()}
+    return render(request, 'tasks/add.html', ctx)
