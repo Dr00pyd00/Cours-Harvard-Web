f0b14783ebdb1c52d35ed9605f9f28b37a54e656
kottak laptop <kevinarnaud974@hotmail.com>
2025-04-20 17:07:49 +0200

    add Flight and Airport, and some stuff templates etc

---
+++ airline/flights/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Flight
+
+# Create your views here.
+
+def index(request):
+    ctx = {'flights': Flight.objects.all()}
+    return render(request, 'flights/index.html', ctx)
+
